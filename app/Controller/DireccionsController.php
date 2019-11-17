108ad259551b0724317f5cdb8175645d6c35e0a7
Ruben <24781752+Heabys@users.noreply.github.com>
2019-11-17 15:07:43 -0500

    Correccion Direccions

--- app/Controller/DireccionsController.php
+++ app/Controller/DireccionsController.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('AppController', 'Controller');
+
 /**
  * Direccions Controller
  *
@@ -9,116 +10,121 @@ App::uses('AppController', 'Controller');
 class DireccionsController extends AppController
 {
 
-	/**
-	 * Components
-	 *
-	 * @var array
-	 */
-	public $components = array('Paginator', 'Session');
+    /**
+     * Components
+     *
+     * @var array
+     */
+    public $components = array('Paginator', 'Session');
 
-	/**
-	 * index method
-	 *
-	 * @return void
-	 */
-	public function index()
-	{
-		$this->Direccion->recursive = 0;
-		$this->set('direccions', $this->Paginator->paginate());
-	}
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $this->Direccion->Behaviors->load('Containable');
+        $this->Direccion->recursive = 1;
+        $options = array(
+            'conditions' => array('Cliente.id' => AuthComponent::user('id'))
 
-	/**
-	 * view method
-	 *
-	 * @throws NotFoundException
-	 * @param string $id
-	 * @return void
-	 */
-	public function view($id = null)
-	{
-		if (!$this->Direccion->exists($id)) {
-			throw new NotFoundException(__('Invalid direccion'));
-		}
-		$options = array('conditions' => array('Direccion.' . $this->Direccion->primaryKey => $id));
-		$this->set('direccion', $this->Direccion->find('first', $options));
-	}
+        );
+        $this->set('direccions', $this->Direccion->find('all', $options));
+    }
 
-	/**
-	 * add method
-	 *
-	 * @return void
-	 */
-	public function add()
-	{
-		if ($this->request->is('post')) {
-			$this->Direccion->create();
-			$cliente=$this->Direccion->Cliente->find('first',array(
-			    'conditions' => array('Cliente.user_id' => AuthComponent::user('id'))
+    /**
+     * view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function view($id = null)
+    {
+        if (!$this->Direccion->exists($id)) {
+            throw new NotFoundException(__('Invalid direccion'));
+        }
+        $options = array('conditions' => array('Direccion.' . $this->Direccion->primaryKey => $id));
+        $this->set('direccion', $this->Direccion->find('first', $options));
+    }
+
+    /**
+     * add method
+     *
+     * @return void
+     */
+    public function add()
+    {
+        if ($this->request->is('post')) {
+            $this->Direccion->create();
+            $cliente = $this->Direccion->Cliente->find('first', array(
+                'conditions' => array('Cliente.user_id' => AuthComponent::user('id'))
             ));
-			$this->request->data['Direccion']['cliente_id']=$cliente['Cliente']['id'];
-			if ($this->Direccion->save($this->request->data)) {
-				$this->Session->setFlash(__('The direccion has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The direccion could not be saved. Please, try again.'));
-			}
-		}
-		$ciudads = $this->Direccion->Ciudad->find('list', array('fields' => array('id', 'nombre')));
-		$clientes = $this->Direccion->Cliente->find('list');
-		$this->set(compact('ciudads', 'clientes'));
-	}
+            $this->request->data['Direccion']['cliente_id'] = $cliente['Cliente']['id'];
+            if ($this->Direccion->save($this->request->data)) {
+                $this->Session->setFlash(__('The direccion has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The direccion could not be saved. Please, try again.'));
+            }
+        }
+        $ciudads = $this->Direccion->Ciudad->find('list', array('fields' => array('id', 'nombre')));
+        $clientes = $this->Direccion->Cliente->find('list');
+        $this->set(compact('ciudads', 'clientes'));
+    }
 
-	/**
-	 * edit method
-	 *
-	 * @throws NotFoundException
-	 * @param string $id
-	 * @return void
-	 */
-	public function edit($id = null)
-	{
-		if (!$this->Direccion->exists($id)) {
-			throw new NotFoundException(__('Invalid direccion'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			$cliente=$this->Direccion->Cliente->find('first',array(
-			    'conditions' => array('Cliente.user_id' => AuthComponent::user('id'))
+    /**
+     * edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function edit($id = null)
+    {
+        if (!$this->Direccion->exists($id)) {
+            throw new NotFoundException(__('Invalid direccion'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            $cliente = $this->Direccion->Cliente->find('first', array(
+                'conditions' => array('Cliente.user_id' => AuthComponent::user('id'))
             ));
-			$this->request->data['Direccion']['cliente_id']=$cliente['Cliente']['id'];
-			if ($this->Direccion->save($this->request->data)) {
-				$this->Session->setFlash(__('The direccion has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The direccion could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('Direccion.' . $this->Direccion->primaryKey => $id));
-			$this->request->data = $this->Direccion->find('first', $options);
-		}
-		$ciudads = $this->Direccion->Ciudad->find('list', array('fields' => array('id', 'nombre')));
-		$clientes = $this->Direccion->Cliente->find('list');
-		$this->set(compact('ciudads', 'clientes'));
-	}
+            $this->request->data['Direccion']['cliente_id'] = $cliente['Cliente']['id'];
+            if ($this->Direccion->save($this->request->data)) {
+                $this->Session->setFlash(__('The direccion has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The direccion could not be saved. Please, try again.'));
+            }
+        } else {
+            $options = array('conditions' => array('Direccion.' . $this->Direccion->primaryKey => $id));
+            $this->request->data = $this->Direccion->find('first', $options);
+        }
+        $ciudads = $this->Direccion->Ciudad->find('list', array('fields' => array('id', 'nombre')));
+        $clientes = $this->Direccion->Cliente->find('list');
+        $this->set(compact('ciudads', 'clientes'));
+    }
 
-	/**
-	 * delete method
-	 *
-	 * @throws NotFoundException
-	 * @param string $id
-	 * @return void
-	 */
-	public function delete($id = null)
-	{
-		$this->Direccion->id = $id;
-		if (!$this->Direccion->exists()) {
-			throw new NotFoundException(__('Invalid direccion'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->Direccion->delete()) {
-			$this->Session->setFlash(__('The direccion has been deleted.'));
-		} else {
-			$this->Session->setFlash(__('The direccion could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
+    /**
+     * delete method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function delete($id = null)
+    {
+        $this->Direccion->id = $id;
+        if (!$this->Direccion->exists()) {
+            throw new NotFoundException(__('Invalid direccion'));
+        }
+        $this->request->allowMethod('post', 'delete');
+        if ($this->Direccion->delete()) {
+            $this->Session->setFlash(__('The direccion has been deleted.'));
+        } else {
+            $this->Session->setFlash(__('The direccion could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(array('action' => 'index'));
+    }
 }
